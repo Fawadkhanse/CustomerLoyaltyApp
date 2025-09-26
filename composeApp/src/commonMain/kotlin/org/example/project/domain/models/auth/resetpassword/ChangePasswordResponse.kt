0fe4f2117e0fe91afa4004436998ebf0be159eff
Fawad Ali <Fawadkhanse@gmail.com>
2025-09-27 00:24:42 +0500

    auth data class setup

--- composeApp/src/commonMain/kotlin/org/example/project/domain/models/auth/resetpassword/ChangePasswordResponse.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/domain/models/auth/resetpassword/ChangePasswordResponse.kt
@@ -1,6 +1,7 @@
 package org.example.project.domain.models.auth.resetpassword
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class ChangePasswordResponse(
